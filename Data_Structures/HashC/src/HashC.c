6da95f1f07f51f41ff1d8c2df205d3490fbd4ca4
Kevin <kpluas21@gmail.com>
2023-04-21 21:20:24 -0400

    Initial commit

---
+++ Data_Structures/HashC/src/HashC.c
@@ -0,0 +1,11 @@
+/**
+ * @file HashC.c
+ * @author your name (you@domain.com)
+ * @brief Implementation for hashmaps in C
+ * @version 0.1
+ * @date 2023-04-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
---
+++ Data_Structures/HashC/src/HashC.h
@@ -0,0 +1,23 @@
+/**
+ * @file HashC.h
+ * @author your name (you@domain.com)
+ * @brief Interface for HashC.c
+ * @version 0.1
+ * @date 2023-04-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+typedef struct HashC HashC;
+
+typedef struct HashC {
+    
+}HashC;
+
+
+void HashC_insert(void);
+
+void HashC_search(void);
+
+void HashC_delete(void);
